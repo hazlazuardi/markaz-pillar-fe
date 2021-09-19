87376bea476a1d10ebcfdab271763e36f3eec120
AbimanyuYudaDewa <abimanyu.yuda@ui.ac.id>
2021-09-19 12:11:19 +0700

    chore : added styling to login page (still not similar to hifi)

--- pages/login.js
+++ pages/login.js
@@ -1,37 +1,119 @@
-import React from 'react'
-import { Grid, Typography, TextField, Button, Divider} from '@material-ui/core'
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+
+function Copyright(props) {
+    return (
+        <Typography variant="body2" color="text.secondary" align="center" {...props}>
+            {'Copyright © '}
+            <Link color="inherit" href="https://material-ui.com/">
+                Your Website
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
+
+const theme = createTheme();
 
 export default function login() {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        // eslint-disable-next-line no-console
+        console.log({
+            email: data.get('email'),
+            password: data.get('password'),
+        });
+    };
+
     return (
         <div>
-            <Grid container style={{ minHeight: "100vh"}}>
-                <Grid item xs={12} sm={6}>
-                    <img src="https://via.placeholder.com/150" style={{ width: "100%", height: "100%", objectFit:"cover"}} alt="brand" />
+            <ThemeProvider theme={theme}>
+      <Grid container component="main" sx={{ height: '100vh' }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage: 'url(https://source.unsplash.com/random)',
+            backgroundRepeat: 'no-repeat',
+            backgroundColor: (t) =>
+              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square >
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            <Typography component="h1" variant="h5">
+              Masuk ke akun anda
+            </Typography>
+            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+                style ={{backgroudColor:"black"}}
+              >
+                Sign In
+              </Button>
+              <Grid container>
+                <Grid item>
+                  <Link href="#" variant="body2">
+                    {"Belum memiliki akun? Registrasi"}
+                  </Link>
                 </Grid>
-                <Grid container item xs={12} sm={6} alignItems="center" direction="column" justifyContent="center" style={{padding:10}}>
-                    <div>
-                        <Grid container justify="center">
-                            <Typography variant={'h4'}>Masuk ke akun anda</Typography>
-                        </Grid>
-                    </div>
-                    <div>
-                        <Button variant="contained">Masuk dengan Google</Button>
-                    </div>
-                    <Divider variant="middle" />
-                    <div>
-                        <TextField required label="Email" variant="outlined" margin="normal"></TextField>
-                    </div>
-                    <div>
-                        <TextField required label="Password" variant="outlined" margin="normal"></TextField>
-                    </div>
-                    <div>
-                        <Button variant="contained">Masuk</Button>
-                    </div>
-                    <div>
-                        <Typography variant="p">Belum memiliki akun? Registrasi</Typography>
-                    </div>
-                </Grid>
-            </Grid>
+              </Grid>
+              <Copyright sx={{ mt: 5 }} />
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
         </div>
-    )
+    );
 }
