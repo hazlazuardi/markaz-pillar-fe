d484822b76c868afa9d340ed2c378332a1692e52
KevinAfiza <kevin.muhammad91@ui.ac.id>
2021-09-23 04:29:38 +0700

    Initialization for my activity

---
+++ pages/myactivity.js
@@ -0,0 +1,12 @@
+//import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import Layout from "../component/layout"
+
+export default function Home() {
+  return (
+  <Layout>
+    <div className={styles.container}>
+    </div>
+    </Layout>
+  )
+}
