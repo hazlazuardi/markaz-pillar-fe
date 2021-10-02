7009b39ee7e0570316f2169a707832f38879db56
AbimanyuYudaDewa <abimanyu.yuda@ui.ac.id>
2021-10-02 23:00:35 +0700

    feat: added detail page for santri

---
+++ pages/data_santri_single.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import DetailTemplate from '../component/templates/detail'
+import Layout from "../component/layout"
+
+export default function data_santri_single() {
+    return (
+        <Layout>
+            <DetailTemplate/>
+        </Layout>
+    )
+}
