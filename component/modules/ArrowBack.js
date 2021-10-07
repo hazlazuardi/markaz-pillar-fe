dfd4fcfd0910d7758e39342c2a617e267dc4489c
AbimanyuYudaDewa <abimanyu.yuda@ui.ac.id>
2021-10-07 22:35:52 +0700

    refactor : broken up detail page into components, implemented dynamic routing for santri

---
+++ component/modules/ArrowBack.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { useTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
+
+export default function ArrowBack(props) {
+    const {name} = props
+
+    const theme = useTheme();
+    const mediumScreen = useMediaQuery(theme.breakpoints.up('md'));
+    const largeScreen = useMediaQuery(theme.breakpoints.up('lg'));
+    return (
+        <Grid item sm={12} lg={12} mb={8} sx={{display: largeScreen? "block":"none"}}>
+            <Typography variant="body1">
+                <ArrowBackIcon/> {name}
+            </Typography>
+        </Grid>
+    )
+}
--- pages/santri/[id].jsx
+++ pages/santri/[id].jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import DetailTemplate from '../component/templates/detail'
-import Layout from "../component/layout"
+import DetailTemplate from '../../component/templates/detail'
+import Layout from "../../component/layout"
 
 export default function data_santri_single() {
     return (
