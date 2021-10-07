db8b12c3be86fe5eee4f2f664a3814c12a21637c
AbimanyuYudaDewa <abimanyu.yuda@ui.ac.id>
2021-10-07 20:56:24 +0700

    refactor: added ellipsis in santri description (1 line)

--- component/modules/Card.js
+++ component/modules/Card.js
@@ -3,43 +3,47 @@ import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import Paper from '@mui/material/Paper';
 import styles from '../../styles/Home.module.css';
 
 export default function Card(props) {
     const {image, santri_name, desc} = props
 
     return (
-        <Grid item sm={12} lg={3}>
-            <Box
-            sx={{
-                width: "auto",
-                height: 250,
-                backgroundImage: 'url(https://source.unsplash.com/random)',
-                backgroundRepeat: 'no-repeat',
-                backgroundColor: (t) =>
-                    t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-                backgroundSize: 'cover',
-                backgroundPosition: 'center'
-            }}
-            />
-            <Grid container spacing={2} className={styles.textCenter}>
-                <Grid item xs={12} mt={2}>
-                    <Typography variant="h6">
-                        {santri_name}
-                    </Typography>
+        <Grid item>
+            <Paper sx={{width: 300, height: 420}}>
+                <Box
+                sx={{
+                    width: "auto",
+                    height: 250,
+                    backgroundImage: 'url(https://source.unsplash.com/random)',
+                    backgroundRepeat: 'no-repeat',
+                    backgroundColor: (t) =>
+                        t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center'
+                }}
+                />
+                <Grid container spacing={2} className={styles.textCenter}>
+                    <Grid item xs={12} mt={2}>
+                        <Typography variant="h6">
+                            {santri_name}
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12} sx={{ width: 300, height:50, overflow: "hidden", textOverflow: "ellipsis"}}>
+                        <Typography
+                            noWrap
+                            p={1}
+                        >
+                            {desc}
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12} sx={{ display:"flex", justifyContent:"space-around" }}>
+                        <Button variant="contained">Donasi</Button>
+                        <Button variant="outlined">Lihat Detail</Button>
+                    </Grid>
                 </Grid>
-                <Grid item xs={12}>
-                    <Typography variant="subtitle2">
-                        {desc}
-                    </Typography>
-                </Grid>
-                <Grid item xs={5}>
-                    <Button variant="contained">Donasi</Button>
-                </Grid>
-                <Grid item xs={7}>
-                    <Button variant="outlined">Lihat Detail</Button>
-                </Grid>
-            </Grid>
+            </Paper>
         </Grid>
     )
 }
