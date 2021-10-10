1ac6c78ff45b42f02c506aa8f8e2c0ee380bd2c0
Wishnu Hazmi <wishnu.hazmi@ui.ac.id>
2021-10-11 05:13:35 +0700

    Pbi 5 mengelola admin markaz

--- src/pages/santri/index.jsx
+++ src/pages/santri/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useCallback } from "react";
 import ShowAllTemplate from "../../component/templates/show_all/ShowAll";
 import Card from "../../component/modules/Card";
 import { useState } from 'react';
@@ -15,54 +15,59 @@ export default function SantriLayout(props) {
 
   const [data, setData] = useState([])
 
-  const getData = async (event) => {
-      await fetch(`${BASE_URL}/santri/search?page=${page}&n=${value}`, {
-      method: 'GET',
-      headers: {
-          'Accept': 'application/json, text/plain, */*',
-          'Content-Type': 'application/json'
-      }
-      }).then(preResponse => {
-          preResponse.json().then(data => {
+  const getData =
+    useCallback(
+      () => {
+        async (event) => {
+          await fetch(`${BASE_URL}/santri/search?page=${page}&n=${value}`, {
+            method: 'GET',
+            headers: {
+              'Accept': 'application/json, text/plain, */*',
+              'Content-Type': 'application/json'
+            }
+          }).then(preResponse => {
+            preResponse.json().then(data => {
               setData(data.result)
+            })
+            console.log(data)
           })
-          console.log(data)
-      })
-  }
-
+        }
+      },
+      [data, page, value],
+    )
   useEffect(() => {
     getData()
-  }, [page,value])
+  }, [getData])
 
-  if(data.length == 0) {
+  if (data.length == 0) {
     return (
       <p>Loading...</p>
     )
   } else {
     return (
-      <ShowAllTemplate 
-        searchBarName=" Cari Santri" 
+      <ShowAllTemplate
+        searchBarName=" Cari Santri"
         markazOrSantri="Santri"
-        page = {page}
-        setPage = {setPage}
+        page={page}
+        setPage={setPage}
         value={value}
         setValue={setValue}
         setSearchTerm={setSearchTerm}
       >
         {data.filter(val => {
-          if(searchTerm == "") {
+          if (searchTerm == "") {
             return val
-          } else if (val.name.toLowerCase().includes(searchTerm.toLowerCase())){
+          } else if (val.name.toLowerCase().includes(searchTerm.toLowerCase())) {
             return val
           }
         }).map((val, key) => (
           <Card
-          key = {val.id} 
-          image={val.thumbnailURL} 
-          name={val.name} 
-          desc={val.background} 
-          intr_1="Donasi" 
-          intr_2="Lihat Detail"
+            key={val.id}
+            image={val.thumbnailURL}
+            name={val.name}
+            desc={val.background}
+            intr_1="Donasi"
+            intr_2="Lihat Detail"
           />
         ))}
       </ShowAllTemplate>
