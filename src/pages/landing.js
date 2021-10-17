6f559e4c9168f7225ac4f41293017d712b5cf2ba
Hanifah Annisa Putri <hanifah.annisa@ui.ac.id>
2021-10-17 13:31:18 +0700

    Create components

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -7,6 +7,11 @@ import { Paper } from '@mui/material';
 import { Grid } from '@mui/material';
 import Button from '@mui/material/Button';
 import useMediaQuery from '@mui/material/useMediaQuery';
+import theme from "../theme"
+import { ThemeProvider } from '@material-ui/styles';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import EmailIcon from '@mui/icons-material/Email';
+import Typography from '@mui/material/Typography';
 
 const useStyles = makeStyles((theme) => ({
   content: {
@@ -35,14 +40,10 @@ const useStyles = makeStyles((theme) => ({
     fontWeight: "bold",
     textTransform: "capitalize"
   },
-  pad: {
-    padding: "0%",
-  },
   pad1: {
-      padding: "5%",
+      padding: "3%",
     },
   email: {
-    padding: "3%",
     textAlign: "center",
 },
 }));
@@ -54,68 +55,115 @@ export default function Home() {
   const largeScreen = useMediaQuery(theme.breakpoints.up('lg'));
 
   return (
-  <>
+  <ThemeProvider theme={theme}>
       <div className={classes.bg}>
       <div className={classes.pad1}>
         <Grid container spacing={0}>
-          <Grid item xs={10} sm={6} md={6} className={classes.heading}>
-            LOGO
-          </Grid>
+          <Grid item xs={10} sm={6} md={6} className={classes.heading}
+          sx={{
+              backgroundImage: 'url(https://source.unsplash.com/random)',
+              backgroundRepeat: 'no-repeat',
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+            }}
+            />
           <Grid item xs={12} sm={6} md={6} className={classes.content}>
+            <Typography>
             <b className={classes.sub}>
-                TENTANG KAMI<br/>
+                TENTANG KAMI<br/><br/>
             </b>
-            <h2>Markaz Pilar</h2><br/>
+            </Typography>
+            <Typography component="h2" variant="h5">Markaz Pilar</Typography><br/>
+            <Typography>
             Markaz Pilar adalah Lorem ipsum dolor sit amet, consectetur adipiscing elit.
             Neque, mauris purus nulla fermentum scelerisque pharetra feugiat.
             Nisl mi maecenas ut et iaculis. Eget lacus, sapien adipiscing in id nam.
             Phasellus purus amet consectetur id ultricies aliquam sed magna habitant.
+            </Typography>
           </Grid>
         </Grid>
 
         <Grid container spacing={0} direction={largeScreen? "row":"column-reverse"}>
           <Grid item xs>
-            <p className={classes.contentCenter}>
-                FOTO<br/><br/>
+            <Typography className={classes.contentCenter}>
+                <Grid item
+                  sx={{
+                      backgroundImage: 'url(https://source.unsplash.com/random)',
+                      backgroundRepeat: 'no-repeat',
+                      backgroundSize: 'cover',
+                      backgroundPosition: 'center',
+                      width: '100px',
+                      height: '100px',
+                      marginLeft: '37%',
+                      marginBottom: '5%'
+                    }}
+                    />
                 <b className={classes.sub}>
                 TENTANG KAMI
                 </b><br/>
                 At eripuit signiferumque sea, vel ad mucius molestie, cu labitur.<br/><br/>
                 <Button variant="contained" className={classes.btn}>Donasi Sekarang</Button>
-            </p>
+            </Typography>
           </Grid>
 
           <Grid item xs>
-            <p className={classes.contentCenter}>
-                FOTO<br/><br/>
+            <Typography className={classes.contentCenter}>
+                <Grid item
+                  sx={{
+                      backgroundImage: 'url(https://source.unsplash.com/random)',
+                      backgroundRepeat: 'no-repeat',
+                      backgroundSize: 'cover',
+                      backgroundPosition: 'center',
+                      width: '100px',
+                      height: '100px',
+                      marginLeft: '37%',
+                      marginBottom: '5%'
+                    }}
+                    />
                 <b className={classes.sub}>
                 TENTANG KAMI
                 </b><br/>
                 At eripuit signiferumque sea, vel ad mucius molestie, cu labitur.<br/><br/>
                 <Button variant="contained" className={classes.btn}>Lihat Markaz</Button>
-            </p>
+            </Typography>
           </Grid>
 
           <Grid item xs>
-            <p className={classes.contentCenter}>
-                FOTO<br/><br/>
+            <Typography className={classes.contentCenter}>
+                <Grid item
+                  sx={{
+                      backgroundImage: 'url(https://source.unsplash.com/random)',
+                      backgroundRepeat: 'no-repeat',
+                      backgroundSize: 'cover',
+                      backgroundPosition: 'center',
+                      width: '100px',
+                      height: '100px',
+                      marginLeft: '37%',
+                      marginBottom: '5%'
+                    }}
+                    />
                 <b className={classes.sub}>
                 TENTANG KAMI
                 </b><br/>
                 At eripuit signiferumque sea, vel ad mucius molestie, cu labitur.<br/><br/>
                 <Button variant="contained" className={classes.btn}>Lihat Santri</Button>
-            </p>
+            </Typography>
           </Grid>
         </Grid>
       </div>
       </div>
-        <Grid container spacing={0} direction={largeScreen? "row":"column-reverse"} className={classes.pad}>
+        <Grid container spacing={0} direction={largeScreen? "row":"column-reverse"}>
           <Grid item xs className={classes.content}>
+            <Typography>
             <b className={classes.sub}>
-                PROGRAM UNTUK MASYARAKAT<br/>
+                PROGRAM UNTUK MASYARAKAT
             </b>
-            <h2>Ikuti berbagai kelas di Markaz Pilar!</h2><br/>
-            <p>Daftarkan diri anda untuk mengikuti berbagai kelas yang akan dipandu oleh para ahli dibidangnya.</p>
+            </Typography>
+            <br/>
+            <Typography component="h2" variant="h5">Ikuti berbagai kelas di Markaz Pilar!</Typography><br/>
+            <Typography>Daftarkan diri anda untuk mengikuti berbagai kelas yang akan dipandu oleh para ahli dibidangnya.</Typography>
+              <br/>
+              <Typography>
               <Grid container spacing={5}>
                   <Grid item xs>
                       <div className={classes.sub}>
@@ -125,6 +173,7 @@ export default function Home() {
                       Lokasi: Masjid UI Depok<br/>
                       Jadwal: Senin, 13.00-14.00<br/>
                       <div className={classes.sub}>
+                      <br/>
                         <b>Lihat program lain →</b><br/>
                       </div>
                   </Grid>
@@ -137,19 +186,32 @@ export default function Home() {
                       Jadwal: Senin, 13.00-14.00
                   </Grid>
               </Grid>
+              </Typography>
           </Grid>
-          <Grid item xs className={classes.heading}>
-            FOTO
-          </Grid>
+          <Grid item xs className={classes.heading}
+          sx={{
+              backgroundImage: 'url(https://source.unsplash.com/random)',
+              backgroundRepeat: 'no-repeat',
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+            }}
+            />
         </Grid>
 
-        <Grid container spacing={0} direction={largeScreen? "row":"column-reverse"} className={classes.pad}>
-          <Grid item xs className={classes.heading}>
-            FOTO
-          </Grid>
+        <Grid container spacing={0} direction={largeScreen? "row":"column-reverse"}>
+          <Grid item xs className={classes.heading}
+          sx={{
+              backgroundImage: 'url(https://source.unsplash.com/random)',
+              backgroundRepeat: 'no-repeat',
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+            }}
+            />
           <Grid item xs className={classes.content}>
-             <h2>Daftarkan diri anda sebagai pengajar di berbagai kelas!</h2><br/>
-             <p>Kami juga membutuhkan lulusan santri yang dapat membantu kami memandu kelas untuk santri tahfidz.</p>
+          <Typography>
+             <Typography component="h2" variant="h5">Daftarkan diri anda sebagai pengajar di berbagai kelas!</Typography><br/>
+             <Typography>Kami juga membutuhkan lulusan santri yang dapat membantu kami memandu kelas untuk santri tahfidz.</Typography>
+              <br/>
               <Grid container spacing={5}>
                   <Grid item xs>
                       <div className={classes.sub}>
@@ -159,6 +221,7 @@ export default function Home() {
                       Lokasi: Masjid UI Depok<br/>
                       Jadwal: Senin, 13.00-14.00<br/>
                       <div className={classes.sub}>
+                      <br/>
                         <b>Lihat program lain →</b><br/>
                       </div>
                   </Grid>
@@ -171,17 +234,23 @@ export default function Home() {
                       Jadwal: Senin, 13.00-14.00
                   </Grid>
               </Grid>
+              </Typography>
           </Grid>
         </Grid>
 
     <div className={classes.bg}>
-        <Grid container spacing={0} direction={largeScreen? "row":"column-reverse"} className={classes.pad}>
+        <Grid container spacing={0} direction={largeScreen? "row":"column-reverse"}>
           <Grid item xs className={classes.content}>
+            <Typography>
             <b className={classes.sub}>
-                PROGRAM UNTUK SANTRI TAHFIDZ<br/>
+                PROGRAM UNTUK SANTRI TAHFIDZ
             </b>
-            <h2>Daftarkan diri Anda sebagai volunteer!</h2><br/>
-            <p>Bantu kami menjalankan berbagai program untuk santri tahfidz dengan mendaftarkan diri anda sebagai volunteer di Markaz Pilar.</p>
+            </Typography>
+            <br/>
+            <Typography component="h2" variant="h5">Daftarkan diri Anda sebagai volunteer!</Typography><br/>
+            <Typography>Bantu kami menjalankan berbagai program untuk santri tahfidz dengan mendaftarkan diri anda sebagai volunteer di Markaz Pilar.</Typography>
+              <br/>
+              <Typography>
               <Grid container spacing={6}>
                   <Grid item xs>
                       <div className={classes.sub}>
@@ -189,7 +258,7 @@ export default function Home() {
                       </div>
                       Lokasi: Hutan UI<br/>
                       Jadwal: Selasa, 14 September 2021<br/>
-                      Jumlah Volunteer: 3<br/>
+                      Jumlah Volunteer: 3<br/><br/>
                       <div className={classes.sub}>
                         <b>Lihat program lain →</b><br/>
                       </div>
@@ -203,28 +272,38 @@ export default function Home() {
                       Jumlah Volunteer: 3
                   </Grid>
               </Grid>
+              </Typography>
           </Grid>
-          <Grid item xs className={classes.heading}>
-            FOTO
-          </Grid>
+          <Grid item xs className={classes.heading}
+          sx={{
+              backgroundImage: 'url(https://source.unsplash.com/random)',
+              backgroundRepeat: 'no-repeat',
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+            }}
+            />
         </Grid>
     </div>
 
+    <Typography>
     <div className={classes.pad1}>
         <div className={classes.sub}>
             <b>HUBUNGI KAMI</b><br/>
         </div>
         <Grid container spacing={0}>
           <Grid item xs={10} sm={6} md={6} className={classes.email}>
-            LOGO<br/>
+            <WhatsAppIcon color='primary'/>
+            <br/>
             +62 123456789
           </Grid>
           <Grid item xs={10} sm={6} md={6} className={classes.email}>
-            LOGO<br/>
+            <EmailIcon color='primary'/>
+            <br/>
             markaz.pillar@gmail.com
           </Grid>
         </Grid>
     </div>
-    </>
+    </Typography>
+    </ThemeProvider>
   )
 }
