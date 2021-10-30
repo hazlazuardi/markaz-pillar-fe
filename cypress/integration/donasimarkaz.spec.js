bbc65c0d6f1c06148a45b563f470d7a9cf1d55d6
Hanifah Annisa Putri <hanifahaputri@gmail.com>
2021-10-31 02:36:31 +0700

    test: added testing for tambah/edit markaz

--- cypress/integration/donasimarkaz.spec.js
+++ cypress/integration/donasimarkaz.spec.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 describe('Test it is in the correct page', () => {
     it('Test if it contains "Create Markaz Donation Detail', () => {
@@ -8,5 +9,16 @@ describe('Test it is in the correct page', () => {
     it('Test if it contains "Create Santri Donation Detail" or not', () => {
         cy.visit('http://localhost:300/admin/markaz/donasi/add/75')
         cy.get('p').contains('Create Santri Donation Detail').should('not.exist')
+=======
+describe('Test it is in the correct page', () => {
+    it('Test if tambaheditmarkaz.js contains "UBAH MARKAZ', () => {
+        cy.visit('http://localhost:3000/tambaheditmarkaz')
+        cy.get('p').contains('UBAH MARKAZ').should('exist')
+    })
+
+    it('Test if tambaheditmarkaz.js contains "UBAH SANTRI" or not', () => {
+        cy.visit('http://localhost:3000/tambaheditmarkaz')
+        cy.get('p').contains('UBAH SANTRI').should('not.exist')
+>>>>>>> 5428a9e (test: added testing for tambah/edit markaz)
     })
 })
