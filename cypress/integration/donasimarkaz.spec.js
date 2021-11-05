70a43252e846486154ad6ebab3cc3e2929125a75
Hanifah Annisa Putri <hanifahaputri@gmail.com>
2021-11-06 04:05:55 +0700

    chore: created layout for daftar transaksi santri

--- cypress/integration/donasimarkaz.spec.js
+++ cypress/integration/donasimarkaz.spec.js
@@ -1,11 +1,12 @@
+
 describe('Test it is in the correct page', () => {
-    it('Test if tambaheditmarkaz.js contains "UBAH MARKAZ', () => {
-        cy.visit('http://localhost:3000/tambaheditmarkaz')
-        cy.get('p').contains('UBAH MARKAZ').should('exist')
+    it('Test if it contains "Create Markaz Donation Detail', () => {
+        cy.visit('http://localhost:3000/admin/markaz/donasi/add/75')
+        cy.get('h5').contains('Create Markaz Donation Detail').should('exist')
     })
 
-    it('Test if tambaheditmarkaz.js contains "UBAH SANTRI" or not', () => {
-        cy.visit('http://localhost:3000/tambaheditmarkaz')
-        cy.get('p').contains('UBAH SANTRI').should('not.exist')
+    it('Test if it contains "Create Santri Donation Detail" or not', () => {
+        cy.visit('http://localhost:3000/admin/markaz/donasi/add/75')
+        cy.get('p').contains('Create Santri Donation Detail').should('not.exist')
     })
 })
