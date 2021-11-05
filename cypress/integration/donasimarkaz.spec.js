83861af628b7931d6b89dfc836fd5081f1077107
Hanifah Annisa Putri <hanifahaputri@gmail.com>
2021-11-05 22:21:41 +0700

    refactor: refactor file positioning

--- cypress/integration/donasimarkaz.spec.js
+++ cypress/integration/donasimarkaz.spec.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 describe('Test it is in the correct page', () => {
     it('Test if it contains "Create Markaz Donation Detail', () => {
@@ -7,18 +6,6 @@ describe('Test it is in the correct page', () => {
     })
 
     it('Test if it contains "Create Santri Donation Detail" or not', () => {
-        cy.visit('http://localhost:300/admin/markaz/donasi/add/75')
+        cy.visit('http://localhost:3000/admin/markaz/donasi/add/75')
         cy.get('p').contains('Create Santri Donation Detail').should('not.exist')
-=======
-describe('Test it is in the correct page', () => {
-    it('Test if tambaheditmarkaz.js contains "UBAH MARKAZ', () => {
-        cy.visit('http://localhost:3000/tambaheditmarkaz')
-        cy.get('p').contains('UBAH MARKAZ').should('exist')
-    })
-
-    it('Test if tambaheditmarkaz.js contains "UBAH SANTRI" or not', () => {
-        cy.visit('http://localhost:3000/tambaheditmarkaz')
-        cy.get('p').contains('UBAH SANTRI').should('not.exist')
->>>>>>> 5428a9e (test: added testing for tambah/edit markaz)
-    })
 })
