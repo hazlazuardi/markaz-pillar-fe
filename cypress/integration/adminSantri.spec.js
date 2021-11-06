23ef206ef59e5a5040a6c3a54ee0334237d83c80
wishnu.hazmi <wishnu.hazmi@ui.ac.id>
2021-11-06 18:13:11 +0700

    test: [RED] test admin santri

---
+++ cypress/integration/adminSantri.spec.js
@@ -0,0 +1,45 @@
+beforeEach(() => {
+    const testEmail = `achmadafriza123@gmail.com`
+    cy.visit('http://localhost:3000/login')
+    cy.get('#email').type(testEmail)
+    cy.get('#password').type('Admin123')
+    cy.get('#submitAtLogin').contains('Masuk').click()
+    cy.wait(2000)
+    cy.visit('http://localhost:3000/admin/santri')
+})
+
+describe('Test it is in the correct page', () => {
+    it('Test if admin markaz page contains "Daftar Santri" or not', () => {
+        cy.visit('http://localhost:3000/admin/santri')
+        cy.get('h4').contains('Daftar Santri').should('exist')
+    })
+
+    it('Test if admin santri page contains "This is admin page" or not', () => {
+        cy.visit('http://localhost:3000/admin/santri')
+        cy.get('p').contains('This is admin page').should('not.exist')
+    })
+
+    it('Test if admin santri page redirect unauthorized users', () => {
+        cy.viewport('iphone-5')
+        cy.get('#menuIconButton').should('exist').click()
+        cy.get('button').contains('Keluar').should('exist').click()
+        cy.visit('http://localhost:3000/admin/santri')
+        cy.url().should('eq', 'http://localhost:3000/', {timeout: 1000})
+    })
+
+})
+
+describe(`Test if all components exist and visible`, () => {
+    it('Test if all exists and visible', () => {
+        cy.get('[data-testid=searchbar-at-admin-markaz]').should('exist')
+        cy.get('[data-testid=filterChipButton-at-admin-markaz]').should('exist')
+        cy.get('[data-testid=tab-grid-at-admin-markaz]').contains('Grid').should('exist')
+        cy.get('[data-testid=gridView-at-admin-markaz]').should('exist')
+        cy.get('[data-testid=tab-table-at-admin-markaz]').contains('Table').should('exist').click()
+        cy.get('[data-testid=tableView-at-admin-markaz]').should('exist')
+        cy.get('[data-testid=pagination-at-admin-markaz]').contains('2').should('exist').click()
+        cy.get('[data-testid=showEntries-at-admin-markaz]').should('exist').click().get('li').contains('50').should('exist').click()
+        cy.get('[data-testid=pagination-at-admin-markaz]').contains('2').should('not.exist')
+        cy.get('[data-testid=fab-at-admin-markaz]').should('exist')
+    });
+})
--- src/pages/admin/santri/index.js
+++ src/pages/admin/santri/index.js
@@ -1,85 +1,13 @@
-import React, { useState } from "react";
-import ShowAllTemplate from "../../../component/templates/show_all/ShowAll";
-import GridView from "../../../component/templates/admin/admin-grid";
-import TableView from "../../../component/templates/admin/admin-table";
-import Link from "@mui/material/Link";
-import Fab from "@mui/material/Fab";
-import AddIcon from "@mui/icons-material/Add";
-import { axiosMain } from "../../../axiosInstances";
-import useSWR from "swr";
+import React from 'react'
 
-const fetcher = url => axiosMain.get(url).then(res => res.data)
-export default function AdminSantri() {
-  const { data: santris, error, mutate } = useSWR("/santri/search?n=1000", fetcher)
-
-  const [gridView, setGridView] = useState(true);
-
-  const [value, setValue] = useState(10);
-
-  const [page, setPage] = useState(0);
-
-  const create = (
-    <Link href="santri/create" underline="none">
-      <Fab
-        sx={{ position: "fixed", right: "3em", bottom: "3em" }}
-        color="primary"
-        aria-label="add"
-      >
-        <AddIcon />
-      </Fab>
-    </Link>
-  );
-
-
-  const handleDelete = async (id) => {
-    await axiosMain.delete(`/admin/santri?id=${id}`)
-      .then(response => {
-        
-        mutate();
-      })
-      .catch(e => {
-        
-        if (e.response.data.status === 401) {
-          localStorage.clear();
-        }
-      })
-  }
-
-  
-  if (error) return "An error has occurred.";
-  if (!santris) return "Loading...";
+function AdminSantri() {
   return (
-    <ShowAllTemplate
-      searchBarName="Cari Markaz"
-      markazOrSantri="Markaz"
-      page={page}
-      setPage={setPage}
-      value={value}
-      setValue={setValue}
-      setSearchTerm={setSearchTerm}
-      add={create}
-      isAdmin
-      setGridView={setGridView}
-    >
-      {gridView ? (
-        <GridView
-          data={santris}
-          intr1Butt="admin/santri/edit"
-          markazOrSantri="admin/santri/delete"
-          detail="admin/santri"
-          handleDelete={handleDelete}
-        />
-      ) : (
-        <TableView
-          data={santris}
-          santriormarkaz="santri"
-          detail="admin/santri"
-          tableTempatMarkaz="Tempat Markaz"
-          tableDomisili="Domisili"
-          tableJenisKelamin="Jenis Kelamin"
-          tableTanggalLahir="Tanggal Lahir"
-        />
-      )}
-    </ShowAllTemplate>
-  );
+    <div>
+      <p>
+        This is admin page
+      </p>
+    </div>
+  )
 }
+
+export default AdminSantri
