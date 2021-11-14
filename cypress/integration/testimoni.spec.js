1e81c63b945f8ff3aad446f2c89563dc7c31af23
Hanifah Annisa Putri <hanifahaputri@gmail.com>
2021-11-14 10:55:44 +0700

    [GREEN] feat: added testimoni volunteer page

--- cypress/integration/testimoni.spec.js
+++ cypress/integration/testimoni.spec.js
@@ -1,11 +1,11 @@
 describe('Test it is in the correct page', () => {
     it('Test if it contains "Create Volunteer Testimoni Detail', () => {
-        cy.visit('http://localhost:3000/admin/volunteer/donasi/create')
+        cy.visit('http://localhost:3000/admin/volunteer/testimoni/create')
         cy.get('h5').contains('Create Volunteer Testimoni Detail').should('exist')
     })
 
     it('Test if it contains "Create Volunteer Donation Detail" or not', () => {
-        cy.visit('http://localhost:3000/admin/volunteer/donasi/create')
+        cy.visit('http://localhost:3000/admin/volunteer/testimoni/create')
         cy.get('p').contains('Create Volunteer Donation Detail').should('not.exist')
     })
 })
---
+++ src/pages/admin/volunteer/testimoni/create.js
@@ -0,0 +1,7 @@
+
+export default function CreateVolunteerTestimoni() {
+
+  return (
+    <h5>Create Volunteer Testimoni Detail</h5>
+  )
+}
