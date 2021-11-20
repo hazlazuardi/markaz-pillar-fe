a72e02975731f529b560bb3888d9598db6445f33
AbimanyuYudaDewa <abimanyu.yuda@ui.ac.id>
2021-11-20 22:26:20 +0700

    feat: added interceptor to axios for token refreshing

--- src/axiosInstances.js
+++ src/axiosInstances.js
@@ -13,6 +13,36 @@ export const axiosMain = axios.create({
     }
 })
 
+axiosMain.interceptors.response.use((response) => {
+    console.log("NO ERROR")
+    return response
+}, 
+function (error) {
+    console.log("ERROR JAAAAAAAAa")
+    const originalRequest = error.config;
+    if (error.response.status === 401 && !originalRequest._retry) {
+        originalRequest._retry = true;
+        return axios.post('/authenticate/refresh',
+            {
+                "refreshToken": localStorage.getItem('currentRefreshToken'),
+                "accessToken": localStorage.getItem('currentAccessToken')
+            })
+            .then(res => {
+                if (res.status === 201) {
+                    // 1) put token to LocalStorage
+                    localStorage.setItem("currentAccessToken", res.data.accessToken);
+                    localStorage.setItem("currentRefreshToken", res.data.refreshToken);
+                    // 2) Change Authorization header
+                    axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('currentAccessToken');
+                    // 3) return originalRequest object with Axios.
+                    console.log("Token Refreshed")
+                    return axios(originalRequest);
+                }
+            })
+    }
+    return Promise.reject(error);
+})
+
 export const axiosFormData = axios.create({
     baseURL: BASE_URL,
     headers: {
