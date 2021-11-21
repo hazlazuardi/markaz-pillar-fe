db351f25a151cc3b462883c264f8792bed4e743d
Al Taaj Kautsar Supangkat <al.taaj@ui.ac.id>
2021-11-21 17:05:29 +0700

    chore: merge with master

--- src/component/modules/FilterComponent.js
+++ src/component/modules/FilterComponent.js
@@ -82,7 +82,7 @@ export default function FilterComponent(props) {
 
   const [open, setOpen] = React.useState(false);
   const anchorRef = React.useRef(null);
-  
+
   const handleToggle = () => {
     setOpen((prevOpen) => !prevOpen);
   };
