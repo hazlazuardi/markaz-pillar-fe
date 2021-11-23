761c5dcb34a2bc86bea6af7a6a27c2b0d0327590
Al Taaj Kautsar Supangkat <al.taaj@ui.ac.id>
2021-11-23 22:45:13 +0700

    feat: add pagination in landing grid view

---
+++ src/component/templates/LandingGridView.js
@@ -0,0 +1,67 @@
+import { Container, Typography } from "@mui/material";
+import GridView from "./admin/GridView";
+import {
+  FormControl,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Pagination,
+  style,
+  Select,
+  Stack,
+  useMediaQuery,
+} from "@mui/material";
+import React, { useCallback, useEffect, useState } from "react";
+
+export default function LandingGridView(props) {
+  const {
+    data,
+    size,
+    page,
+    setPage,
+    markazOrSantri,
+    intr1Butt,
+    detail,
+    handleDelete,
+  } = props;
+
+  // *******************************************************
+  // Pagination
+  // *******************************************************
+  const matchXs = useMediaQuery("(max-width:600px)");
+  const handlePagination = useCallback(
+    (event, value) => {
+      setPage(value);
+      // disable it until API Calls done
+      setDoAnimateHeight(false);
+    },
+    [setPage]
+  );
+
+  return (
+    <Container>
+      <Typography>
+        <b style={{ color: "#004F5D" }}>
+          Daftar Program Relawan <br />
+          <br />
+        </b>
+      </Typography>
+      <Grid sx={{ p: 1}}>
+        <GridView data={data} />
+      </Grid>
+      {/* Pagination */}
+      {!!data && data.totalElement !== 0 && (
+        <Stack sx={{ bottom: "0em" }} spacing={2} alignItems="center">
+          <Pagination
+            data-testid="pagination-at-admin-or-user-template"
+            size={matchXs ? "small" : "medium"}
+            boundaryCount={1}
+            count={data.totalPage}
+            page={page}
+            onChange={handlePagination}
+          />
+        </Stack>
+      )}
+    </Container>
+  );
+}
